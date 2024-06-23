124fbd38a1b4264bf5c4572f229d0867e439d4bb
sangambansode45 <bansodesangam23@gmail.com>
2024-06-23 19:58:18 +0530

    Add files via upload

---
+++ Xylophone app/ViewController.swift
@@ -0,0 +1,28 @@
+
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func keyButtonPrssed(_ sender: UIButton) {
+        
+        playSound(soundName: sender.currentTitle!)
+    }
+    
+    
+    func playSound(soundName:String) {
+        let url = Bundle.main.url(forResource:soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf:url!)
+        player.play()
+    
+    }
+}
+
